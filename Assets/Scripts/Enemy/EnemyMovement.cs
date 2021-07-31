3ee08292cc9ed3fa6d5907161a5ca872cd30e40f
CameronChildress <cameronchildress4@gmail.com>
2021-07-30 20:49:48 -0600

    Enemy Attacks

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -67,6 +67,8 @@ public class EnemyMovement : MonoBehaviour
 
     public void HandleMoveToTarget()
     {
+        if (enemyManager.isPerformingAction) return;
+
         Vector3 targetDirection = currentTarget.transform.position - transform.position;
         distanceFromTarget = Vector3.Distance(currentTarget.transform.position, transform.position);
         float viewableAngle = Vector3.Angle(targetDirection, transform.forward);
