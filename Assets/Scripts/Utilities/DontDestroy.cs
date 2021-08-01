460906eac8033f8910ec237581ac0f93e452032b
JSchmidt1004 <67611141+JSchmidt1004@users.noreply.github.com>
2021-08-01 15:19:00 -0600

    Fixed menu duplicates
    Still have the major error with reloading after exiting the game

--- Assets/Scripts/Utilities/DontDestroy.cs
+++ Assets/Scripts/Utilities/DontDestroy.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class DontDestroy : MonoBehaviour
 {
+    public bool allowDuplicates = false;
+
     void Awake()
     {
+        if (!allowDuplicates)
+        {
+            Object[] gameObjects = GameObject.FindObjectsOfType(typeof(DontDestroy));
+            List<Object> matchingObjects = new List<Object>();
+
+            foreach(Object gameObject in gameObjects)
+            {
+                if (gameObject.name == name) matchingObjects.Add(gameObject);
+            }
+
+            if (matchingObjects.Count > 1) return;
+        }
+
         DontDestroyOnLoad(this);   
     }
 
     public void ResetDontDestroy()
     {
-        //gameObject.transform.SetParent(GameObject.Find("DontDestroyOnLoad")?.transform);
         DontDestroyOnLoad(this);
     }
 }
