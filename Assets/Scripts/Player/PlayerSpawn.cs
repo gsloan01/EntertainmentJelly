1c86f7c472fd1f252b955c9f64efacf2b5d7c59d
Jesse Whiting <little.dude.jesse@gmail.com>
2021-08-01 10:57:54 -0600

    Police shutdown

--- Assets/Scripts/Player/PlayerSpawn.cs
+++ Assets/Scripts/Player/PlayerSpawn.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerSpawn : MonoBehaviour
 {
+
+
     void Start()
     {
         FPSPlayer player = FindObjectOfType<FPSPlayer>();
@@ -11,6 +13,8 @@ public class PlayerSpawn : MonoBehaviour
         {
             player.gameObject.transform.position = transform.position;
             player.gameObject.transform.rotation = transform.rotation;
+
+            player.gameObject.transform.localScale = transform.localScale;
         }
     }
 }
